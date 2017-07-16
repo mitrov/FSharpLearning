f0eea01a80bdaee4d8cf58795f5eb8615b84e3f6
Dzoka Mitrov <d.mitrov@gmail.com>
2017-07-16 18:37:25 +0200

    Initial Commit - Files in Sequence

---
+++ Eigene Übungen/Files in Sequence/Files In Sequence/Files In Sequence/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace Files_In_Sequence.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("Files In Sequence")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("Files In Sequence")>]
+[<assembly: AssemblyCopyright("Copyright ©  2017")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("cc4a3f86-6e97-4bee-91b2-2cd257d53f53")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ Eigene Übungen/Files in Sequence/Files In Sequence/Files In Sequence/Program.fs
@@ -0,0 +1,25 @@
+﻿open System.IO
+
+let getFiles path =
+    try
+        Directory.GetFiles path |> Seq.ofArray
+    with _ -> Seq.empty
+
+let getDirectories path =
+    try
+        Directory.GetDirectories path |> Seq.ofArray
+    with _ -> Seq.empty
+        
+let rec allFilesUnder basePath  =
+    seq {
+        yield! getFiles basePath
+
+        for subdir in getDirectories basePath do
+        yield! allFilesUnder subdir
+    }
+
+[<EntryPoint>]
+let main argv = 
+    allFilesUnder @"C:\"
+    |> Seq.iter(printfn "%s")
+    0 // return an integer exit code
